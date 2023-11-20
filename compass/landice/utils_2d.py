67911bd9f4da9d5dfc783ad508a7f6dbcc05962f
Cameron Smith <smithc11@rpi.edu>
2023-11-20 15:39:47 -0500

    don't render grid or vertices

--- compass/landice/utils_2d.py
+++ compass/landice/utils_2d.py
@@ -4,7 +4,6 @@
 
 import math
 
-from compass.landice.common import frange
 from compass.landice.settings import CELL_SIZE, XMAX, XMIN, YMAX, YMIN
 
 
@@ -22,7 +21,7 @@ def element(e, **kwargs):
     """Utility function used for rendering svg"""
     s = "<" + e
     for key, value in kwargs.items():
-        s += " {}='{}'".format(key, value)
+        s += " {}='{}'".format(key, value[0])
     s += "/>\n"
     return s
 
@@ -40,34 +39,11 @@ def make_svg(file, edges, f, xmin=XMIN, xmax=XMAX, ymin=YMIN, ymax=YMAX,
                        (ymax - ymin) * scale))
 
     file.write("<g transform='scale({})'>\n".format(scale))
-    # Draw grid
-    styleSpec = "stroke: grey; stroke-width: 0.02; fill: none"
-    for x in frange(xmin, xmax, cellSize):
-        for y in frange(ymin, ymax, cellSize):
-            file.write(element("rect", x=x, y=y, width=cellSize,
-                               height=cellSize,
-                               style=styleSpec))
-
-    # Draw filled / unfilled circles
-    styleSpec = "stroke: black; stroke-width: 0.02; fill: "
-    for x in frange(xmin, xmax + cellSize, cellSize):
-        for y in frange(ymin, ymax + cellSize, cellSize):
-            is_solid = f(x, y) > 0
-            fill_color = ("black" if is_solid else "white")
-            file.write(element("circle", cx=x, cy=y, r=0.05,
-                               style=styleSpec + fill_color))
     # Draw edges
     for edge in edges:
         file.write(element("line", x1=edge.v1.x, y1=edge.v1.y,
                            x2=edge.v2.x, y2=edge.v2.y,
                            style='stroke:rgb(255,0,0);stroke-width:0.04'))
-
-    # Draw vertices
-    r = 0.05
-    for v in [v for edge in edges for v in (edge.v1, edge.v2)]:
-        file.write(element("rect", x=(v.x - r), y=(v.y - r),
-                           width=2 * r, height=2 * r,
-                           style='fill: red'))
 
     file.write("</g>\n")
     file.write("</svg>\n")
